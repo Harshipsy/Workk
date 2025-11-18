fc60d8ade37a5f67e4102424d692f7f7ed95dea6
Harshipsy <harshhvaghela3@gmail.com>
2025-11-18 18:31:05 +0530

    app.py

--- app.py
+++ app.py
@@ -40,11 +40,13 @@ app.config["SECRET_KEY"] = "ABC"
 #                POSTGRES CONFIGURATION
 # -----------------------------------------------------
 db_url = os.environ.get("DATABASE_URL")
-
 if db_url and db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
+    db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
+elif db_url and db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
@@ -673,6 +675,7 @@ def export():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
